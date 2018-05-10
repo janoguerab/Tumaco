86919fc7ac548e1904dc69cf3ab9a3a1eac42d40
Alejandro Noguera <janoguerab@unal.edu.co>
2018-05-10 17:35:47 -0500

    Entrevista: I. IDENTIFICACIÓN DEL ESTUDIANTE Y MOTIVO DE CONSULTA

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,7 +6,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatStepperModule} from '@angular/material/stepper';
-
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -27,6 +33,13 @@ import { EntrevistaComponent } from './entrevista/entrevista.component';
       MatInputModule,
       MatButtonModule,
       MatStepperModule,
+      MatCardModule,
+      MatToolbarModule,
+      MatIconModule,
+      MatTabsModule,
+      MatSelectModule,
+      MatRadioModule,
+      MatCheckboxModule
 
   ],
   providers: [],
--- front/src/app/entrevista/entrevista.component.ts
+++ front/src/app/entrevista/entrevista.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-entrevista',
@@ -10,6 +11,8 @@ export class EntrevistaComponent {
     isLinear = false;
     firstFormGroup: FormGroup;
     secondFormGroup: FormGroup;
+    now = new Date();
+    asesor="NOMBRE ASESOR";
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -21,4 +24,34 @@ export class EntrevistaComponent {
     });
   }
 
+  progCurricular = new FormControl();
+  tipoProgCurricular: string;
+  tipoProgCurricularSel:string;
+  progCurricularGrupos = [
+    {
+      name: 'Pregrado',
+      programa: [
+        { value: 'pregrado-0', viewValue: 'Administración de Empresas' },
+        { value: 'pregrado-1', viewValue: 'Antropología' },
+        { value: 'pregrado-2', viewValue: 'Arquitectura ' }
+      ]
+    },
+    {
+      name: 'Posgrado',
+      programa: [
+        { value: 'posgrado-1', viewValue: 'Doctorado en Agroecología' },
+        { value: 'posgrado-2', viewValue: 'Doctorado en Antropología' },
+        { value: 'posgrado-3', viewValue: 'Doctorado en Arte y Arquitectura' }
+      ]
+    }
+  ];
+
+  tipoIngreso=['Regular','PAES','PEAMA','Foráneo','Intercambio'];
+  tipoVivienda=['Familiar','Arriendo','Propia','Residencia universitaria'];
+tipoResidencia=['Rural','Urbano'];
+tipoSexo=['H','M','I'];
+tipoIdentidadGenero=['H','M','T'];
+tipoOrientacionSexual=['L','G','B','H','I'];
+tipoAsesoria=['Académica','Administrativa','Habilidades para el aprendizaje','Psicosocial','Soporte justificativo' ];
+tipoAcademica=['Cancelación de semestre','Cancelación de asignatura','Académicos - reingreso','Carga mínima','Traslado','Iniciativas estudiantiles','Doble titulación','Homologaciones, convalidaciones y equivalencias','Habilidades para el aprendizaje']
 }
